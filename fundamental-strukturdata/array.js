0b1a752a9d5131e8060a5b11fe2690a27878ad59
wawanneutron <wawanneutron1331@gmail.com>
2022-05-15 10:37:06 +0700

    struktur data rray

---
+++ fundamental-strukturdata/array.js
@@ -0,0 +1,18 @@
+const fruits = ["grapes", "star fruit", "cherry", "orange"];
+// menambahkan coconut di  index ke 4
+fruits.push("coconut");
+/* 
+step: 
+1. membuat item kosong di indek ke 4
+2. pindahkan orange ke index ke 4
+3. pindahkan cherry ke index ke 3
+4, tambahkan coconut ke index ke 2
+*/
+
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[3]);
+console.log(fruits[4]);
+
+console.log(fruits);
